5d18acdbe70299d306a4bf48419f578ba8a1e0fa
karseny99 <arseny.karimov@gmail.com>
2025-11-01 12:16:01 +0300

    lab6

---
+++ lab6/tld.py
@@ -0,0 +1,15 @@
+from copy import deepcopy
+import numpy as np
+
+def tridiagonal_method(a, b, c, d):
+    n = len(d)
+    x=np.zeros(n)
+    p = [-c[0] / b[0]]
+    q = [d[0] / b[0]]
+    for i in range(1, n):
+        p.append(-c[i] / (b[i] + a[i] * p[i - 1]))
+        q.append((d[i] - a[i] * q[i - 1]) / (b[i] + a[i] * p[i - 1]))
+    x[-1] = q[-1]
+    for i in reversed(range(n-1)):
+        x[i] = p[i] * x[i + 1] + q[i]
+    return x
